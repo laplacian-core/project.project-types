a3eb3bb0d7c843474f3c02c1674a3bd496018976
iwauo <iwauo.tajima@gmail.com>
2020-04-20 18:59:17 +0900

    add basic project types

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# laplacian.project.project-types
+<!-- @head-content@ -->
+# laplacian/project.project-types
+
 The basic project types used in the Laplacian project.
+
+<!-- @head-content@ -->
+
+<!-- @toc@ -->
+## Table of contents
+1. [Usage](#usage)
+
+
+1. [Source list](#source-list)
+
+
+
+<!-- @toc@ -->
+
+<!-- @main-content@ -->
+## Usage
+
+Add the following entry to your project definition.
+```yaml
+project:
+  models:
+  - group: laplacian
+    name: project.project-types
+    version: 1.0.0
+```
+
+
+
+
+## Source list
+
+
+[README.md](<./README.md>)
+
+[.editorconfig](<./.editorconfig>)
+
+[.gitattributes](<./.gitattributes>)
+
+[.gitignore](<./.gitignore>)
+
+[model/project/sources.yaml](<./model/project/sources.yaml>)
+
+[model/project.yaml](<./model/project.yaml>)
+
+[src/project_types/generator.yaml](<./src/project_types/generator.yaml>)
+
+[src/project_types/model.yaml](<./src/project_types/model.yaml>)
+
+[src/project_types/schema-model.yaml](<./src/project_types/schema-model.yaml>)
+
+[src/project_types/schema-plugin.yaml](<./src/project_types/schema-plugin.yaml>)
+
+[src/project_types/template.yaml](<./src/project_types/template.yaml>)
+
+
+
+
+
+<!-- @main-content@ -->
---
+++ scripts/publish-local.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+set -e
+SCRIPT_BASE_DIR=$(cd $"${BASH_SOURCE%/*}" && pwd)
+PROJECT_BASE_DIR=$(cd $SCRIPT_BASE_DIR && cd .. && pwd)
+
+GRADLE_DIR=${SCRIPT_BASE_DIR}/build/laplacian
+GRADLE_BUILD_FILE="$GRADLE_DIR/build.gradle"
+GRADLE_SETTINGS_FILE="$GRADLE_DIR/settings.gradle"
+
+REMOTE_REPO_PATH='https://raw.github.com/nabla-squared/mvn-repo/master/'
+LOCAL_REPO_PATH="$PROJECT_BASE_DIR/../mvn-repo"
+if [[ -d "$PROJECT_BASE_DIR/subprojects/mvn-repo" ]]
+then
+  LOCAL_REPO_PATH="$PROJECT_BASE_DIR/subprojects/mvn-repo"
+fi
+
+DEST_DIR="$PROJECT_BASE_DIR/dest"
+
+main() {
+  generate
+  publish
+
+}
+
+## @generate-function@ ##
+generate() {
+  $SCRIPT_BASE_DIR/generate.sh
+}
+## @generate-function@ ##
+
+## @publish-function@ ##
+publish() {
+  create_settings_gradle
+  create_build_gradle
+  run_gradle
+}
+
+run_gradle() {
+  (cd $GRADLE_DIR
+    ./gradlew \
+      --stacktrace \
+      --build-file build.gradle \
+      --settings-file settings.gradle \
+      --project-dir $GRADLE_DIR \
+      publish
+  )
+}
+
+create_settings_gradle() {
+  cat <<EOF > $GRADLE_SETTINGS_FILE
+pluginManagement {
+    repositories {
+        maven {
+            url '${LOCAL_REPO_PATH}'
+        }
+        maven {
+            url '${REMOTE_REPO_PATH}'
+        }
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+rootProject.name = "laplacian.project.project-types"
+EOF
+}
+
+create_build_gradle() {
+  cat <<EOF > $GRADLE_BUILD_FILE
+plugins {
+    id 'maven-publish'
+    id 'org.jetbrains.kotlin.jvm' version '1.3.70'
+}
+
+group = 'laplacian'
+version = '1.0.0'
+
+repositories {
+    maven {
+        url '${LOCAL_REPO_PATH}'
+    }
+    maven {
+        url '${REMOTE_REPO_PATH}'
+    }
+    jcenter()
+}
+
+task moduleJar(type: Jar) {
+    from '${DEST_DIR}'
+}
+
+publishing {
+    repositories {
+        maven {
+            url '${LOCAL_REPO_PATH}'
+        }
+    }
+    publications {
+        mavenJava(MavenPublication) {
+            artifact moduleJar
+        }
+    }
+}
+EOF
+}
+## @publish-function@ ##
+
+main
